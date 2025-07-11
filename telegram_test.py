71dcd8a1def0d2d79004ca6af1b6b7bfd88da34c
bengleizes <benoit.gleizes@univ-reunion.fr>
2025-07-11 16:36:41 +0400

    Create telegram_test.py

---
+++ telegram_test.py
@@ -0,0 +1,16 @@
+import os
+import requests
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message
+    }
+    response = requests.post(url, data=payload)
+    print(f"[Telegram] Réponse : {response.status_code} - {response.text}")
+
+send_telegram_message("📢 Test direct depuis Railway avec telegram_test.py")
